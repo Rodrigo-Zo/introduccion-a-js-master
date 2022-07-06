6ff4c3373067e54e3decc4e65994a790cbeeb903
Rodrigo <zoccorodrigo@gmail.com>
2022-07-06 16:45:12 -0300

    Elimina textos innecesarios

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -1,10 +1,3 @@
-/*
-TAREA: Empezar preguntando cuánta gente hay en el grupo familiar.
-Crear tantos inputs+labels como gente haya para completar la edad de cada integrante.
-Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad, la menor edad y el promedio del grupo familiar.
-
-Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
-*/
 function obtenerNumeroMasGrande(array){
     let numeroMasGrande = -Infinity;
     for(let i = 0; i < array.length; i++){
@@ -108,17 +101,6 @@ document.querySelector('#boton-resetear').onclick = function (){
     eliminarIntegrante();
     ocultarElemento(document.querySelector('#trabajos-y-salarios'));
 }
-
-/*
-TAREA:
-Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar 
-el salario anual de cada integrante de la familia que trabaje.
-Al hacer click en "calcular", mostrar en un elemento pre-existente el mayor salario anual,
-menor salario anual, salario anual promedio y salario mensual promedio.
-
-Punto bonus: si hay inputs vacíos, ignorarlos en el cálculo (no contarlos como 0).
-*/
-ocultarElemento(document.querySelector('#trabajos-y-salarios'));
 
 function crearSalarioIntegrante(){
     let $salarioIntegrante = document.createElement('div');
